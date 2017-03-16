982a1298e895110ea32979318a3051e0cfb9d215
Vladimir Guzman <guzman.vla@gmail.com>
2017-03-16 10:41:26 -0500

    identation

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "welcome/index.html.erb", type: :view do
 
   it "should see 'Hello World!'" do
-  	visit root_path
+    visit root_path
     expect(page).to have_content("Hello World!")
   end
 end
