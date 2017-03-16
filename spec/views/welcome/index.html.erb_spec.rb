9f3e4f2e00e67bbf992e601826e7dc34f5046c34
Vladimir Guzman <guzman.vla@gmail.com>
2017-03-16 10:18:04 -0500

    test runner

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "welcome/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should see 'Hello World!'" do
+  	visit root_path
+    expect(page).to have_content("Hello World!")
+  end
 end
